cf1f827ce2be1de63d066667f4b438dbae934b3a
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2019-09-07 16:23:38 -0300

    inicia práctico 1

---
+++ Practico01/Practico1.m
@@ -0,0 +1,33 @@
+#### EJERCICIO 1 ####
+s = 1 + 2
+fun = sin(pi / 4)
+a = log(a^-1)
+
+who
+
+#### EJERCICIO 2 ####
+a = [1 2 3]
+b = [1, 2, 3]
+c = [1; 2; 3]
+
+size(a)
+size(b)
+size(c)
+
+help size
+
+#### EJERCICIO 3 ####
+A = [2 4; 8 7]
+B = [0 -1; -1 0]
+C = [4 7 6; 1 9 6; 3 2 3]
+
+D = A + B
+A'
+A^-1
+A*B
+A.*B
+
+(A+i*B)'
+(A+i*B).'
+
+
