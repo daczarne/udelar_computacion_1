e603fe4a2cdce07ec79b5ce885ae76eecde7e866
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2019-09-08 17:51:49 -0300

    ejercicio 10

--- Practico01/Practico1.m
+++ Practico01/Practico1.m
@@ -70,3 +70,68 @@ A = [2 -1; 1 10; 1 2]
 b = ones(3, 1)
 x = A \ b
 
+#### EJERCICIO 6 ####
+
+### Parte b ###
+x = 1:5
+y = [0 .25 3 1.5 2]
+z = y .^ 2
+plot(x, y, 'b-', x, z, 'r-')
+legend('Y', 'Z')
+
+#### EJERCICIO 7 ####
+
+### Parte b ###
+x = 0:.1:2*pi
+plot(x, sin(x))
+
+### Parte c ###
+help clc
+help clear
+help clf
+
+#### EJERCICIO 8 ####
+plot(x, log(1./x))
+plot(x, exp(cos(2*x*pi/40)))
+
+plot(x, exp(cos(2*x*pi/40)))
+hold on
+plot(x, -log(1./x))
+
+# Approx. 1.8
+tita = [0:.1:2*pi]
+r = 10
+x = r .* cos(tita)
+y = r .* sin(tita)
+
+plot(x, y)
+hold on
+plot(x, (5 - 2 .* x) / 3)
+
+#### EJERCICIO 10 ####
+# a) y d)
+n = [0.3, -0.3, 1.5, 0.98, -0.98, 2.4+3.6i]
+resultados = []
+for j = 1:length(n)
+  resultados(j,:) = [round(n(j)), ceil(n(j)), fix(n(j)), floor(n(j))];
+endfor
+resultados
+
+# b)
+# Para los números negativos fix y ceil se comportarán igual (ambas redondean hacia la derecha). Para los 
+# números positivos se comportarán distinto dado que ceil redondea hacia +Inf (la derecha) mientras que 
+# fix redondeará hacia cero (la izquierda).
+
+# c)
+# El comportamiento de fix y floor será el inverso que fix y ceil. Para los positivos se comportarán igual
+# (redondearán hacia la izquierda), mientras que para los negativos se comportaran distintos. fix redondeará
+# hacia cero (la derecha), mientras que floor redondeará hacia -Inf (la izquierda).
+
+# d)
+# Aplican la función a la parte real e imaginaria por separado
+# round(n): round (real (n)) + round (imag (n)) * I
+# ceil(n): ceil (real (n)) + ceil (imag (n)) * I
+# fix(n): fix (real (n)) + fix (imag (n)) * I
+# floor(n): floor (real (n)) + floor (imag (n)) * I
+
+
