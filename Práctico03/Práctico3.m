70b139416b27a067f60626e2744cab1d50049af9
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2019-09-16 22:35:21 -0300

    ejercicios 5, 6, y 7

--- Práctico03/Práctico3.m
+++ Práctico03/Práctico3.m
@@ -111,9 +111,112 @@ endif
 
 #### EJERCICIO 5 ####
 
+### Caso a)
+x = 2; % s = []
+x = 3; % s = 4
+x = 0.5; % s = 1, falta el intervalo 3
+x = 5.7; % s = 2, falta el intervalo 4
+x = -4; % s = []
+x = 6; % s = 2, falta el intervalo 4
+s = [];
+if ((x >= -1) & (x<= 1))
+  s = [s 1];
+elseif ((x >= 5) & (x<= 7))
+  s = [s 2];
+elseif ((x >= 0) & (x<= 1))
+  s = [s 3];
+elseif ((x >= 3) & (x<= 6))
+  s = [s 4];
+endif
+
+### Caso b)
+x = 2; % s = []
+x = 3; % s = 4
+x = 0.5; % s = 3, falta el intervalo 1
+x = 5.7; % s = 4, falta el intervalo 2
+x = -4; % s = []
+x = 6; % s = 4, falta el intervalo 2
+s = [];
+if ((x >= 3) & (x<= 6))
+  s = [s 4];
+elseif ((x >= 5) & (x<= 7))
+  s = [s 2];
+elseif ((x >= 0) & (x<= 1))
+  s = [s 3];
+elseif ((x >= -1) & (x<= 1))
+  s = [s 1];
+endif
+
+### Caso c)
+x = 2; % s = []
+x = 3; % s = 4
+x = 0.5; % s = [3 1]
+x = 5.7; % s = [4 2]
+x = -4; % s = []
+x = 6; % s = [4 2]
+s = [];
+if ((x >= 3) && (x<= 6))
+  s = [s 4];
+endif
+if ((x >= 5) && (x<= 7))
+  s = [s 2];
+endif
+if ((x >= 0) && (x<= 1))
+  s = [s 3];
+endif
+if ((x >= -1) && (x<= 1))
+  s = [s 1];
+endif
+
 #### EJERCICIO 6 ####
+v = [5, 6, -3, 0, 7, -4, 5, 8, 9];
+n = 8;
+suma = 0;
+i = 1;
+indicador = 0;
+while (i <= n) & ~indicador
+  numero = v(i);
+  if numero > 0
+    suma = suma + numero;
+  else
+    if numero == 0
+      indicador = 1;
+    endif
+  endif
+  i = i + 1;
+endwhile
+disp(suma); % suma = 11
+disp(numero) % numero = 0
 
 #### EJERCICIO 7 ####
+
+### Caso a)
+for b = 1:3
+  if b <= 1
+    a = b - 1;
+  endif
+  if b <= 2
+    a = a - 1;
+  else
+    a = a + 1;
+  endif
+  disp(a);
+endfor % a = -1; a = -2; a = -1
+
+### Caso b)
+bajo = 1;
+for k = bajo:3
+  bajo = bajo + 2;
+  disp(k); % k = 1, k = 2, k = 3
+  disp(bajo); % bajo = 3, bajo = 5, bajo = 7
+endfor
+
+### Caso c)
+alto = 4;
+for k = alto:-1:3
+  disp(k); % k = 4, k = 3
+  disp(alto); % alto = 4, alto = 4
+endfor
 
 #### EJERCICIO 8 ####
 
