ea9cfb24454ec8bfbe88b127851e6c1eb5e42580
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2019-10-26 02:02:00 -0300

    pr01ej01

---
+++ Soluciones_en_Python/Práctico01/Pr01Ej01.py
@@ -0,0 +1,13 @@
+#### PRÁCTICO 01 - EJERCICIO ####
+
+# importamos el modulo math y la funcion sin()
+import math
+
+s = 1 + 2
+print(f"s: {s}")
+
+fun = math.sin(math.pi / 4)
+print(f"fun: {fun}")
+
+a = math.log(s ** (-1))
+print(f"a: {a}")
