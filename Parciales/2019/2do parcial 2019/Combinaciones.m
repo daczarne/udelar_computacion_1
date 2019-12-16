68132ab8792fd15d6a4346cb6fb86a16b4a72c76
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2019-12-16 00:25:36 -0300

    problema 5

--- Parciales/2019/2do parcial 2019/Combinaciones.m
+++ Parciales/2019/2do parcial 2019/Combinaciones.m
@@ -3,11 +3,12 @@
 function M = Combinaciones(k)
   if (k == 0)
     M = [];
+  elseif k == 1
+    M = [0, 1];
   else
-    M = [0,1];
+    M = [0, 1];
+    for i = 2:k
+      M = [zeros(1, size(M,2)), ones(1, size(M,2)); M M];
+    endfor
   endif
-  
-  for i = 2:k
-    M = [zeros(1, size(M,2)), ones(1, size(M,2)); M M];
-  endfor
 endfunction
